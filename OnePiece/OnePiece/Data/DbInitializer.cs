63ba67d352cf0050e68efe0c4db75c9b839c631a
HungerKiller <zhongshui.wang.chn@gmail.com>
2018-08-30 23:11:23 +0800

    add complex data initialization

--- OnePiece/OnePiece/Data/DbInitializer.cs
+++ OnePiece/OnePiece/Data/DbInitializer.cs
@@ -1,11 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using OnePiece.Models;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace OnePiece.Data
 {
@@ -18,6 +17,7 @@ namespace OnePiece.Data
             //WriteJson("Weapons.json", context.Weapons.ToList().Cast<object>().ToList());
             //WriteJson("PirateGroups.json", context.PirateGroups.ToList().Cast<object>().ToList());
             //WriteJson("Persons.json", context.Persons.ToList().Cast<object>().ToList());
+            //WriteJson("PersonOwn.json", GetPersonOwnObjects(context).Cast<object>().ToList());
 
             // Initialize DB by reading json file
             context.Database.EnsureCreated();
@@ -67,27 +67,35 @@ namespace OnePiece.Data
                 }
                 context.SaveChanges();
             }
-            //// TODO Add FruitPossessions
-            //if (!context.FruitPossessions.Any())
-            //{
-            //    var person = context.Persons.FirstOrDefault();
-            //    var fruit = context.Fruits.FirstOrDefault();
-            //    person.FruitPossessions = new List<FruitPossession>();
-            //    person.FruitPossessions.Add(new FruitPossession { PersonID = person.Id, FruitID = fruit.Id });
-            //    context.SaveChanges();
-            //}
-            //// TODO Add WeaponPossessions
-            //if (!context.WeaponPossessions.Any())
-            //{
-            //    var person = context.Persons.FirstOrDefault();
-            //    var weapon = context.Weapons.FirstOrDefault();
-            //    person.WeaponPossessions = new List<WeaponPossession>();
-            //    person.WeaponPossessions.Add(new WeaponPossession { PersonID = person.Id, WeaponID = weapon.Id });
-            //    context.SaveChanges();
-            //}
+            // Add FruitPossessions, WeaponPossessions, PersonPirateGroup
+            if (!context.FruitPossessions.Any())
+            {
+                var fruits = context.Fruits.ToList();
+                var weapons = context.Weapons.ToList();
+                var pirateGroups = context.PirateGroups.ToList();
+
+                var personOwns = ReadPersonOwnFromJson("Data/Files/PersonOwn.json");
+                foreach (PersonOwn po in personOwns)
+                {
+                    var person = context.Persons.SingleOrDefault(p => p.Name == po.PersonName);
+                    foreach (var fruitName in po.OwnedFruitNames)
+                    {
+                        var fruit = fruits.SingleOrDefault(f => f.Name == fruitName);
+                        context.FruitPossessions.Add(new FruitPossession { PersonID = person.Id, FruitID = fruit.Id });
+                    }
+                    foreach (var weaponName in po.OwnedWeaponNames)
+                    {
+                        var weapon = weapons.SingleOrDefault(w => w.Name == weaponName);
+                        context.WeaponPossessions.Add(new WeaponPossession { PersonID = person.Id, WeaponID = weapon.Id });
+                    }
+                    var group = context.PirateGroups.SingleOrDefault(pg => pg.Name == po.PirateGroupName);
+                    person.PirateGroupID = group?.Id;
+                }
+                context.SaveChanges();
+            }
         }
 
-        #region Helper
+        #region Helper DB to Json
 
         public static void WriteJson(string jsonFilePath, List<object> items)
         {
@@ -97,6 +105,29 @@ namespace OnePiece.Data
                 sw.Write(jArray.ToString());
             }
         }
+
+        public static List<PersonOwn> GetPersonOwnObjects(OnePieceContext context)
+        {
+            var persons = context.Persons.AsNoTracking()
+                .Include(p => p.FruitPossessions).ThenInclude(fp => fp.Fruit)
+                .Include(p => p.WeaponPossessions).ThenInclude(wp => wp.Weapon)
+                .Include(p => p.PirateGroup);
+            List<PersonOwn> personOwns = new List<PersonOwn>();
+            foreach (var person in persons)
+            {
+                PersonOwn personOwn = new PersonOwn();
+                personOwn.PersonName = person.Name;
+                personOwn.OwnedFruitNames = person.FruitPossessions.Select(fp => fp.Fruit.Name).ToList();
+                personOwn.OwnedWeaponNames = person.WeaponPossessions.Select(wp => wp.Weapon.Name).ToList();
+                personOwn.PirateGroupName = person.PirateGroup?.Name;
+                personOwns.Add(personOwn);
+            }
+            return personOwns;
+        }
+
+        #endregion Helper DB to Json
+
+        #region Helper Json to DB
 
         public static List<PirateGroup> ReadPirateGroupFromJson(string jsonFilePath)
         {
@@ -142,6 +173,17 @@ namespace OnePiece.Data
             return persons;
         }
 
-        #endregion Helper
+        public static List<PersonOwn> ReadPersonOwnFromJson(string jsonFilePath)
+        {
+            string jsonPersonOwns = null;
+            using (StreamReader sr = new StreamReader(jsonFilePath))
+            {
+                jsonPersonOwns = sr.ReadToEnd();
+            }
+            List<PersonOwn> personOwns = JsonConvert.DeserializeObject<List<PersonOwn>>(jsonPersonOwns);
+            return personOwns;
+        }
+
+        #endregion Helper Json to DB
     }
 }
---
+++ OnePiece/OnePiece/Models/PersonOwn.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace OnePiece.Models
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class PersonOwn
+    {
+        public string PersonName { get; set; }
+
+        public List<string> OwnedFruitNames { get; set; }
+
+        public List<string> OwnedWeaponNames { get; set; }
+
+        public string PirateGroupName { get; set; }
+    }
+}
